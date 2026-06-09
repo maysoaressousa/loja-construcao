2b89ad6292fc2237d3d172531f51b6e65881f087
Mayara Soares <141755987+maydreadful@users.noreply.github.com>
2026-06-09 14:12:12 -0300

    editando nome errado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,23 @@ export default function Home() {
       <section className="bg-azul-marca text-white py-12 px-4 text-center md:py-24 md:px-8 relative overflow-hidden">
         <div className="max-w-3xl mx-auto space-y-6 relative z-10">
           <span className="inline-block text-laranja-principal font-bold uppercase tracking-wider text-xs bg-laranja-principal/10 px-3 py-1.5 rounded-md">
-            Do alicerce au acabamento
+            Do alicerce <div class="accordion" id="accordionExample">
+              <div class="card">
+                <div class="card-header" id="headingOne">
+                  <h2 class="mb-0">
+                    <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      Collapsible Group Item #1
+                    </button>
+                  </h2>
+                </div>
+            
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  </div>
+                </div>
+              </div>
+            </div> acabamento
           </span>
           
           <h1 className="text-3xl font-black tracking-tight leading-tight md:text-6xl">
