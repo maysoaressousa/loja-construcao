f56a87e3dcea5acc87335fa30200c2a313ffb923
Mayara Soares <141755987+maydreadful@users.noreply.github.com>
2026-06-16 16:49:53 -0300

    alterações categorias

--- src/pages/Categorias.jsx
+++ src/pages/Categorias.jsx
@@ -1,7 +1,7 @@
 import { FaTools, FaWrench, FaLightbulb, FaTint, FaBorderAll, FaLayerGroup } from 'react-icons/fa';
 
 export default function Categorias() {
-  const numeroTelefone = "55585987133705";
+  const numeroTelefone = "5585987133705";
 
   const listaCategorias = [
     {
@@ -9,42 +9,42 @@ export default function Categorias() {
       nome: "Material Básico",
       icone: <FaLayerGroup />,
       desc: "Cimento, tijolos, areia, brita, ferro e ferragens estruturais.",
-      imageUrl: "/src/assets/material-basico.png", // Substitua pelo caminho correto da imagem
+      imageUrl: "/src/public/material-basico.png", // Substitua pelo caminho correto da imagem
     },
     {
       id: 2,
       nome: "Hidráulica",
       icone: <FaTint />,
       desc: "Tubos, conexões, caixas d'água, registros e reparos.",
-      imageUrl: "/src/assets/hidraulica.png", // Substitua pelo caminho correto da imagem
+      imageUrl: "/src/public/hidraulica.png", // Substitua pelo caminho correto da imagem
     },
     {
       id: 3,
       nome: "Elétrica",
       icone: <FaLightbulb />,
       desc: "Fios, cabos, disjuntores, tomadas, interruptores e iluminação.",
-      imageUrl: "/src/assets/eletrica.png", // Substitua pelo caminho correto da imagem
+      imageUrl: "/src/public/eletrica.png", // Substitua pelo caminho correto da imagem
     },
     {
       id: 4,
       nome: "Ferramentas",
       icone: <FaWrench />,
       desc: "Ferramentas manuais e elétricas, discos de corte e EPIs.",
-      imageUrl: "/src/assets/ferramentas.png", // Substitua pelo caminho correto da imagem
+      imageUrl: "/src/public/ferramentas.png", // Substitua pelo caminho correto da imagem
     },
     {
       id: 5,
       nome: "Pisos e Revestimentos",
       icone: <FaBorderAll />,
       desc: "Cerâmicas, porcelanatos, argamassas e rejuntes.",
-      imageUrl: "/src/assets/pisos-e-revestimentos.png", // Substitua pelo caminho correto da imagem
+      imageUrl: "/src/public/pisos-e-revestimentos.png", // Substitua pelo caminho correto da imagem
     },
     {
       id: 6,
       nome: "Tintas e Acabamento",
       icone: <FaTools />,
       desc: "Tintas imobiliárias, massas, texturas, rolos e pincéis.",
-      imageUrl: "/src/assets/tintas.png", // Substitua pelo caminho correto da imagem
+      imageUrl: "/src/public/tintas.png", // Substitua pelo caminho correto da imagem
     },
   ];
 
